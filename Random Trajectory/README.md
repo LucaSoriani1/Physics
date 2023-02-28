26336619ac3b74c003c207ae8001cd91af18e9b2
Luca Soriani <102147516+LucaSoriani1@users.noreply.github.com>
2023-02-28 23:33:08 +0100

    Modify README

--- Random Trajectory/README.md
+++ Random Trajectory/README.md
@@ -1,22 +1,22 @@
-Random Trajectories
+##### Random Trajectories
 This program generates and plots random trajectories. A random trajectory is a sequence of positions or events that occur randomly or in a random way. In this program, we generate random trajectories by randomly choosing the direction of movement (left or right) at each step. We then plot these trajectories using the Matplotlib library.
 
-Usage
+### Usage
 To run the program, open the terminal and type the following command:
 
-Copy code
 python random_trajectories.py
 Five random trajectories will be generated and plotted.
 
-Dependencies
+### Dependencies
 The program requires the installation of the following Python libraries:
 
 numpy
 matplotlib
 scipy
+
 The libraries can be installed using pip, for example:
 
 pip install -r requirements.txt
 
-Modifications and Contributions
+### Modifications and Contributions
 This program can be extended and modified to suit different purposes, such as modeling natural processes or generating random data for testing purposes. If you would like to contribute to the program or propose modifications, please send a pull request or contact the author.
--- Random Walk/random_walk.py
+++ Random Walk/random_walk.py
@@ -4,67 +4,51 @@ from matplotlib import pyplot as plt
 from scipy.stats import norm
 import math
 
-
-
-
-step=int(input('How many step do you want?   '))
-
-ris = np.zeros((2*step+1,2))
-
-jump=(-1,1)
-
-
-incr = 0
-for x in range (2*step+1):
-    ris[x][0] = -step + incr
-    incr += 1
-
-limit=200000
-N=1
-while N<=limit:
-    x=0
-    for y in range (step):
-        random_step = random.choice(jump)
-        x += random_step
-        
-    ris[x+step][1] += 1
+def simulate_walk(step, limit):
     
-    N+=1
+    walks = np.zeros((2*step+1, 2))
+    
+    walks[:, 0] = np.arange(-step, step+1)
+    
+    for _ in range(limit):
+        x = np.random.choice([-1,1], size=step).sum()
+        walks[x+step,1] += 1
 
-ris[:,1] = ris[:,1]/limit
+    walks[:,1] /= limit
+    
+    return walks
 
+def plot_distribution(step, walks):
+    """
+    Plot the resulting probability distribution and compare it with a normal distribution.
+    """
+    mu = 0
+    sigma = step**0.5
+    
+    x1=-step
+    x2=step
+
+    z1=((x1-mu)/sigma)-100
+    z2=((x2-mu)/sigma)+100
+
+    x1 = np.arange(z1,z2,0.001)
+    nd = math.sqrt(2)/math.sqrt(np.pi*sigma*sigma) * np.exp(-(x1*x1)/(2*sigma*sigma))
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_xlim([-step-2, step+2])
+    ax.plot(x1, nd, color='red')
+    ax.bar(walks[:,0], walks[:,1], 1.8)
+    ax.set_xlabel('Position, with l=1')
+    ax.set_ylabel('Probability')
+    ax.set_title('Probability distribuiton with N = %s step' %step)
+    plt.show()
+    step=15
+
+STEP = 15
+LIMIT = 200000
+
+# Simulazione del random walk e plot della distribuzione di probabilità
+walks = simulate_walk(STEP, LIMIT)
 np.set_printoptions(precision=5)
-
-print('After', limit, 'cycles, the results are: \n', ris)
-
-prob = 0
-for g in range (2*step+1):
-     prob += ris[g][1]
-
-print('') 
-print('The total probability is:', prob)
-
-mu = 0
-sigma = math.sqrt(step)
-x1=-step
-x2=step
-
-z1=((x1-mu)/sigma)-100
-z2=((x2-mu)/sigma)+100
-
-x1 = np.arange(z1,z2,0.001)
-nd = math.sqrt(2)/math.sqrt(np.pi*sigma*sigma) * np.exp(-(x1*x1)/(2*sigma*sigma))
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.set_xlim([-step-2, step+2])
-ax.plot(x1, nd, color='red')
-ax.bar(ris[:,0], ris[:,1], 1.8)
-ax.set_xlabel('Position, with l=1')
-ax.set_ylabel('Probability')
-ax.set_title('Probability distribuiton with N = %s step' %step)
-plt.show()
-
-
-
-
-
+print(f"After {LIMIT} cycles, the results are:\n{walks}")
+plot_distribution(STEP, walks)
